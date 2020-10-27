02dc6214fb24652ca41f3cf325bf6cd0cf1c5daf
Eliezer <elimasyrubi@gmail.com>
2020-10-27 10:45:22 -0300

    validacion del input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Pregunta from './Components/Pregunta'
 
 function App() {
+
+  //definir state
+  const [presupuestp, guardarPresupuesto] = useState(0)
+  const [restante, guardarRestante] = useState(0)
+
   return (
     <div className="container">
       <header>
@@ -9,7 +14,10 @@ function App() {
       </header>
 
      <div className='contenido-principal contenido'>
-     <Pregunta/>
+     <Pregunta
+        guardarPresupuesto={guardarPresupuesto}
+        guardarRestante={guardarRestante}
+     />
      </div>
 
     </div>
