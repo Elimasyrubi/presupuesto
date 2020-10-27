d4b3e29e4ea65f244554ff14f10f1844327d91b9
Eliezer <elimasyrubi@gmail.com>
2020-10-27 10:14:32 -0300

    solicitud de presupuesto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Pregunta from './Components/Pregunta'
 
 function App() {
   return (
-    <h1>Presupuesto</h1>
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+      </header>
+
+     <div className='contenido-principal contenido'>
+     <Pregunta/>
+     </div>
+
+    </div>
   );
 }
 
